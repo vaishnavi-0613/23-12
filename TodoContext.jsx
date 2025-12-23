c5384a6beb6b245cddea3b50d9ef9e95ad8829ad
vaishnavi-0613 <vaishnavibeenaveni@gmail.com>
2025-12-23 22:28:14 +0530

    Create TodoContext.jsx

---
+++ TodoContext.jsx
@@ -0,0 +1,24 @@
+import { createContext, useState } from "react";
+
+export const TodoContext = createContext();
+
+export const TodoProvider = ({ children }) => {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (title) => {
+    setTodos((prev) => [
+      ...prev,
+      { id: Date.now(), title }
+    ]);
+  };
+
+  const deleteTodo = (id) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== id));
+  };
+
+  return (
+    <TodoContext.Provider value={{ todos, addTodo, deleteTodo }}>
+      {children}
+    </TodoContext.Provider>
+  );
+};
